8fa3dd34b9aa3df1c9294f54e91a4da27f9c9238
Nick Falbo <68254882+nfalbo213@users.noreply.github.com>
2022-11-08 11:53:19 -0500

    whitespace changes

--- modules/animation-images.js
+++ modules/animation-images.js
@@ -1,35 +1,14 @@
 /*
 Copyright 2022 Nick Falbo
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+SPDX-License-Identifier: Apache-2.0
 */
 
-//////////////////
-// Imports
-//import { canvas } from "./canvasDisplay.js";
-
-//let riderImg;
 let riderUpImg;
 let riderDownImg;
 let treeImg;
 let snowmanImg;
 let logImg;
 let rockImg;
-
-
-// new Image(width, height)
-
-/*
-riderImg = new Image((canvas.width * .05), (canvas.height * .05));
-riderImg.src = "./images/GoofyRider.png";
-*/
 
 riderUpImg = new Image(100, 140);
 riderUpImg.src = "./images/compressed/c_RiderUp2.png";
