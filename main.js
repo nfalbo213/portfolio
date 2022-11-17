1cd582b413c172bb62ce35a354d2b16bc727c05d
Nick Falbo <68254882+nfalbo213@users.noreply.github.com>
2022-11-17 13:00:49 -0500

    added footer.js for seperation of concerns

--- main.js
+++ main.js
@@ -96,7 +96,6 @@ form.addEventListener('submit', handleSubmit);
 
 ///////////////////
 // Invocations
-// Change date on footer
-document.getElementById("footer-year").innerHTML = new Date().getFullYear();
+
 // Run main animation
 mainAnimation();
---
+++ modules/footer.js
@@ -0,0 +1,2 @@
+// Change date on footer
+document.getElementById("footer-year").innerHTML = new Date().getFullYear();
